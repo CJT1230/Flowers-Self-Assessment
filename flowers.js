18b912eab414d534e1641a0c63fcc248705a0084
Caleb Thompson <calebjthompson1230@gmail.com>
2021-11-23 15:07:26 -0600

    Added function which adds the ability to push new flowers with an id

--- flowers.js
+++ flowers.js
@@ -14,20 +14,13 @@ const flowers = [
 ]
 
 const addFlower = (flowerObject) => {
-
-    /*
-        This function should add an `id` property
-        to the object that was sent to the function.
-        The value of the `id` property should be 1
-        greater than the current maximum `id` in the
-        array.
-
-        Once the `id` property has been added to the,
-        object, the object should then be put into the
-        `flowers` array.
-    */
-
-
+    const lastIndex = flowers.length - 1
+    const currentLastFlower = flowers[lastIndex]
+    const maxId = currentLastFlower.id
+    const idForNewFlower = maxId + 1
+    
+    flowerObject.id = idForNewFlower
+    flowers.push(flowerObject)
 }
 
 
